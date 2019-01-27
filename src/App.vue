b1c78030abce1e417ce00ceea3ff61d83642b133
guohongjian <2844735407@qq.com>
2019-01-27 15:59:49 +0800

    vue利用xslx插件本地读取excel表格数据

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!--<img alt="Vue logo" src="./assets/logo.png">-->
+    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <div>hello world</div>
+    <!--上传excel-->
+    <input type="file" accept=" application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" id="upload_excel" @change="handleFile($event)"/>
+    <!--展示excel-->
+    <div id="content"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+ import XLSX from 'xlsx'
+// import HelloWorld from './components/HelloWorld.vue'
+/* eslint-disable */
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+    data () {
+        return {
+          fileList: [],  // 获取存本地的数据
+          workBook: ''
+        }
+    },
+    methods: {
+      handleFile (event) {
+          console.log('查看', event.target.files)
+          var fileReader = new FileReader()
+          // 将excel数据转换成二进制再利用xslx插件来转换成js对象
+          fileReader.readAsBinaryString(event.target.files[0])
+          fileReader.onload = (e) => {
+              var data = e.target.result
+              this.workBook = XLSX.read(data, {
+                  type: 'binary'
+              })
+              this.fileList = XLSX.utils.sheet_to_json(this.workBook.Sheets[this.workBook.SheetNames[0]])
+              console.log(this.fileList)
+          }
+    }
   }
 }
 </script>
